2a809e82c1da4ebf97cc2052a3914a85782ed779
lolithaa999 <lolithay7@gmail.com>
2025-03-13 16:23:17 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello, PES1UG22CS308\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "I have successfully built and run PES1UG22CS308 :)\n";
   return 0;
 }
